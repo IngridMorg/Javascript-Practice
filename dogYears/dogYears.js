18ed40c29593899efd2a49a0832b89fde47731b0
IngridMorg <griddymog@gmail.com>
2021-12-24 17:20:32 +0000

    Can now calculate my age in dog years

--- dogYears/dogYears.js
+++ dogYears/dogYears.js
@@ -1,1 +1,14 @@
 //dog years project from codecademy
+//variable storing my age
+const myAge = 19;
+//changing variable for early years
+let earlyYears = 2;
+earlyYears *= 10.5;
+//subtract 2 from myAge as we have accounted for this in early years
+let laterYears = myAge -2;
+//multiply by 4 because apparently this is how dog years work
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+
+const myAgeInDogYears = earlyYears + laterYears;
