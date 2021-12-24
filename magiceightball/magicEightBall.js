8a0d687a19f12a0be7df260591ec55fe517e9534
IngridMorg <griddymog@gmail.com>
2021-12-24 20:06:23 +0000

    Random number generator with in interesting technique to restrict the values generated

--- magiceightball/magicEightBall.js
+++ magiceightball/magicEightBall.js
@@ -3,3 +3,6 @@ userName === 'Jane' ? console.log(`Hello, ${userName}!`) : console.log('Hello!')
 
 let userQuestion = 'What will father christmas get me for christmas?';
 console.log(userQuestion);
+//to make the value range from 0 to 8 we can multiply the return value by 8
+//to then restrict down to 7 we round down with floor
+let randomNumber = Math.floor(Math.random()*8);
