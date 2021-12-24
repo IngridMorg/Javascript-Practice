9bf5d57d8c6846c3f86a1330cb269f3fce61d06e
IngridMorg <griddymog@gmail.com>
2021-12-24 21:19:42 +0000

    COMPLETE CODE - prpject finished big control if else statement

--- raceday/raceDay.js
+++ raceday/raceDay.js
@@ -1,7 +1,16 @@
 let raceNumber = Math.floor(Math.random() * 1000);
-let registeredEarly = false;
-let runnerAge = 19;
+let registeredEarly = true;
+let runnerAge = 18;
 
 if(runnerAge > 18 && raceNumber >= 1000){
     registeredEarly = true;
+}
+if(runnerAge>18 && registeredEarly === true){
+    console.log(`${raceNumber} runs at 9:30am.`);
+} else if(runnerAge > 18 && registeredEarly === false){
+    console.log(`${raceNumber} runs at 11:00am.`);
+} else if(runnerAge < 18){
+    console.log(`${raceNumber} runs at 12:30pm`);
+}else{
+    console.log(`${raceNumber} please see the registration desk.`);
 }
