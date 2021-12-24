ab0c89882c6d25aa2e400b2abd19f767b0f90c0c
IngridMorg <griddymog@gmail.com>
2021-12-24 20:03:47 +0000

    Second print statement.

--- magiceightball/magicEightBall.js
+++ magiceightball/magicEightBall.js
@@ -1,2 +1,5 @@
 let userName = '';
 userName === 'Jane' ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+let userQuestion = 'What will father christmas get me for christmas?';
+console.log(userQuestion);
