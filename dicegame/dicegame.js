33180049264aef7b1f3f97259fadfdd9c98b9426
IngridMorg <griddymog@gmail.com>
2021-12-24 16:01:33 +0000

    created a function but it is not detected when the program runs - need to fix

---
+++ dicegame/dicegame.js
@@ -0,0 +1,31 @@
+//based on the project provided in codecademy (but i do not have access to because
+//i refuse to give in to codeademy's capitalist narrative )
+
+//dice game
+//roll two dice
+//sum the dice
+//print?
+//if we want to get a random value within a range we
+//need to create our own function.
+function randomInt(min, max){
+    min = Math.ceil();
+    max = Math.ceil();
+    return Math.floor(Math.random() * (max - min)+min);
+    //code from:
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+}
+
+let dice1 = Math.randomInt(0,6);
+
+let dice2 = Math.randomInt(0,6);
+
+if(dice1 > dice2){
+    console.log('dice 1 is the winner!\n');
+}
+else if(dice2 > dice1){
+    console.log('dice 2 is the winner!\n');
+}
+else{
+    console.log('Draw!');
+}
+console.log(`dice 1: ${dice1}\ndice 2: ${dice2}\nTotal: `+(dice1 + dice2));
